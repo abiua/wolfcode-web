4a17d263132b4f6fc2634f2c0ec4323807899235
abiu <742765966@qq.com>
2023-06-08 01:18:16 +0800

    2023/06/07 wmb 物流功能：修改完一部分添加功能

--- web/src/main/resources/static/scripts/order/orderInfo/add.js
+++ web/src/main/resources/static/scripts/order/orderInfo/add.js
@@ -27,4 +27,62 @@ layui.use(['form', 'layer'], function () {
         return false;
     });
 
+
+    form.on('select(custId-select-filter)',function (data){
+        var value = data.value;
+        $.ajax({
+            url: web.rootPath()+'custLinkManInfo/listByCustomerId/?custId='+value,
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: "JSON",
+
+
+            //成功or错误后
+            success: function (d){
+                console.log("success");
+                $("#linkman").empty();
+                $("#linkPhone").empty();
+                var optionHtml = `<option value="">---请选择---</option>`
+                if (d.data.length > 0){
+                    console.log("大于0")
+                    d.data.forEach(item=>{
+                        optionHtml += `<option value="${item.id}">${item.linkman}</option>`
+                    })
+                }
+                $("#linkman").html(optionHtml)
+
+                form.render('select','component-form-element')
+
+
+            },error: function (data){
+                console.log("error")
+            }
+        })
+    });
+
+    form.on('select(linkman-select-filter)',function (data){
+        var value = data.value;
+        $.ajax({
+            url: web.rootPath()+'custLinkManInfo/getLinkmanPhone/?id='+value,
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: "JSON",
+
+
+            //成功or错误后
+            success: function (d){
+                console.log("success");
+                $("#linkPhone").empty();
+                var optionHtml = `<option value="${d.data}">${d.data}</option>`
+
+                $("#linkPhone").html(optionHtml)
+
+                form.render('select','component-form-element')
+
+
+            },error: function (data){
+                console.log("error")
+            }
+        })
+    });
 });
